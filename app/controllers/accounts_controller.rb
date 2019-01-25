2fb5ce45843ad422f8fb7ce6a940a53d98166544
Carl Tablante <carl.q.tablante@gmail.com>
2019-01-25 16:24:09 +0800

    Proper JSON Response on login failed

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,12 +1,25 @@
 class AccountsController < ApplicationController
-  def authenticate
-    account = Account.find_by(email: params[:email])
+  before_action :get_account
 
-    if account.authenticate(params[:password])
-      payload = account.to_json(only: [:id, :email])
+  def authenticate
+    if @account.authenticate(params[:password])
+      payload = @account.to_json(only: [:id, :email])
+      @profiles = @account.profiles
+      @groups = @account.groups
       @jwt_token = JWT.encode(payload, Rails.application.credentials.secret_key_base, "HS256")
     else
       # error
+      render json: { message: "email or password incorrect" }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def get_account
+    @account = Account.find_by(email: params[:email])
+
+    if @account.nil?
+      render json: { message: "incorrect email or password" }, status: :unauthorized
     end
   end
 end
