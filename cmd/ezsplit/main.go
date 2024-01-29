35b6f1b803a63e006080a70c34b7153d03738051
Carl Tablante <carlwilliam.tablante@gmail.com>
2024-01-30 09:02:24 +1100

    fix: Removed multiple main package

--- cmd/ezsplit/main.go
+++ cmd/ezsplit/main.go
@@ -1,28 +1,39 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
 	"net/http"
 
-	"github.com/99designs/gqlgen/graphql"
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/carlqt/ezsplit/graph"
+	"github.com/carlqt/ezsplit/internal"
 )
 
+const defaultPort = "8080"
+
 func main() {
+	app := internal.NewApp()
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	port := app.Config.Port
+	if port == "" {
+		port = defaultPort
+	}
 
-	srv.AroundOperations(func(ctx context.Context, next graphql.OperationHandler) graphql.ResponseHandler {
-		oc := graphql.GetOperationContext(ctx)
-		fmt.Printf("around: %s %s", oc.OperationName, oc.RawQuery)
-		return next(ctx)
-	})
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{Repositories: app.Repositories}}))
 
+	// srv.AroundOperations(func(ctx context.Context, next graphql.OperationHandler) graphql.ResponseHandler {
+	// 	oc := graphql.GetOperationContext(ctx)
+	// 	log.Printf("around: %s %s", oc.OperationName, oc.RawQuery)
+	// 	return next(ctx)
+	// })
+
+	// TODO: Remove the playground handler in production.
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+
+	// http.Handle("/query", handlers.CombinedLoggingHandler(os.Stdout, srv))
 	http.Handle("/query", srv)
 
-	fmt.Println("Server running on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
--- go.mod
+++ go.mod
@@ -8,13 +8,10 @@ require (
 	github.com/vektah/gqlparser/v2 v2.5.11
 )
 
-require github.com/felixge/httpsnoop v1.0.3 // indirect
-
 require (
 	github.com/agnivade/levenshtein v1.1.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
-	github.com/gorilla/handlers v1.5.2
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.3 // indirect
 	github.com/lib/pq v1.10.9
