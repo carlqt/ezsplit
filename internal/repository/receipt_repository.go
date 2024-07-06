e9a48b7a2ed44b138b1034c2da90a1a3cee610fe
Carl Tablante <carlwilliam.tablante@gmail.com>
2024-07-07 09:31:47 +1000

    feat: Refactoring receipts

--- internal/repository/receipt_repository.go
+++ internal/repository/receipt_repository.go
@@ -3,13 +3,11 @@ package repository
 import (
 	"database/sql"
 	"fmt"
-	"log/slog"
-	"strconv"
-	"time"
 
 	_ "github.com/lib/pq"
 
-	g "github.com/carlqt/ezsplit/.gen/ezsplit_dev/public/table"
+	"github.com/carlqt/ezsplit/.gen/ezsplit_dev/public/model"
+	. "github.com/carlqt/ezsplit/.gen/ezsplit_dev/public/table"
 	. "github.com/go-jet/jet/v2/postgres"
 )
 
@@ -17,60 +15,38 @@ type ReceiptRepository struct {
 	DB *sql.DB
 }
 
-type Receipt struct {
-	UserID      string    `db:"user_id"`
-	ID          string    `db:"id"`
-	Total       int       `db:"total"`
-	Description string    `db:"description"`
-	CreatedAt   time.Time `db:"created_at"`
-}
+func (r *ReceiptRepository) Create(receipt model.Receipts) error {
+	stmt := Receipts.INSERT(
+		Receipts.Total, Receipts.Description, Receipts.UserID,
+	).VALUES(receipt.Total, receipt.Description, receipt.UserID).RETURNING(Receipts.ID)
 
-func (r *ReceiptRepository) Create(receipt *Receipt) error {
-	err := r.DB.QueryRow("INSERT INTO receipts (total, description, user_id) VALUES ($1, $2, $3) RETURNING id", receipt.Total, receipt.Description, receipt.UserID).Scan(&receipt.ID)
+	err := stmt.Query(r.DB, &receipt)
 	if err != nil {
-		slog.Error(err.Error())
-		return err
+		return fmt.Errorf("failed to create the user: %w", err)
 	}
+
 	return nil
 }
 
-func (r *ReceiptRepository) CreateForUser(receipt *Receipt) error {
-	err := r.DB.QueryRow("INSERT INTO receipts (total, description, user_id) VALUES ($1, $2, $3) RETURNING id", receipt.Total, receipt.Description, receipt.UserID).Scan(&receipt.ID)
+func (r *ReceiptRepository) CreateForUser(receipt model.Receipts) error {
+	stmt := Receipts.INSERT(
+		Receipts.Total, Receipts.Description, Receipts.UserID,
+	).VALUES(receipt.Total, receipt.Description, receipt.UserID).RETURNING(Receipts.ID)
+
+	err := stmt.Query(r.DB, &receipt)
 	if err != nil {
-		slog.Error(err.Error())
-		return err
+		return fmt.Errorf("failed to create the user: %w", err)
 	}
+
 	return nil
 }
 
-func (r *ReceiptRepository) SelectAll() ([]*Receipt, error) {
-	rows, err := r.DB.Query("SELECT id, total, description, created_at FROM receipts")
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	var receipts []*Receipt
-	for rows.Next() {
-		receipt := &Receipt{}
-		err := rows.Scan(&receipt.ID, &receipt.Total, &receipt.Description, &receipt.CreatedAt)
-		if err != nil {
-			slog.Error(err.Error())
-			return nil, err
-		}
-		receipts = append(receipts, receipt)
-	}
-
-	return receipts, nil
-}
-
-func (r *ReceiptRepository) SelectForUser(userID string) ([]*Receipt, error) {
-	intUserID, _ := strconv.ParseInt(userID, 10, 64)
+func (r *ReceiptRepository) SelectForUser(userID string) ([]model.Receipts, error) {
 	stmt := SELECT(
-		g.Receipts.ID, g.Receipts.Total, g.Receipts.Description, g.Receipts.CreatedAt,
-	).FROM(g.Receipts.Table).WHERE(g.Receipts.UserID.EQ(Int(intUserID)))
+		Receipts.ID, Receipts.Total, Receipts.Description, Receipts.CreatedAt,
+	).FROM(Receipts.Table).WHERE(Receipts.UserID.EQ(RawInt(userID)))
 
-	receipts := []*Receipt{}
+	receipts := []model.Receipts{}
 	err := stmt.Query(r.DB, &receipts)
 	if err != nil {
 		return nil, fmt.Errorf("failed to retrieve user receipts from DB: %w", err)
@@ -79,11 +55,16 @@ func (r *ReceiptRepository) SelectForUser(userID string) ([]*Receipt, error) {
 	return receipts, nil
 }
 
-func (r *ReceiptRepository) FindByID(id string) (*Receipt, error) {
-	receipt := &Receipt{}
-	err := r.DB.QueryRow("SELECT id, total, description, created_at FROM receipts WHERE id = $1", id).Scan(&receipt.ID, &receipt.Total, &receipt.Description, &receipt.CreatedAt)
+func (r *ReceiptRepository) FindByID(id string) (model.Receipts, error) {
+	receipt := model.Receipts{}
+
+	stmt := SELECT(
+		Receipts.ID, Receipts.Total, Receipts.Description, Receipts.CreatedAt,
+	).FROM(Receipts.Table).WHERE(Receipts.UserID.EQ(RawInt(id)))
+
+	err := stmt.Query(r.DB, &receipt)
 	if err != nil {
-		return nil, fmt.Errorf("could not find receipt with id %s: %w", id, err)
+		return receipt, fmt.Errorf("could not find receipt with id %s: %w", id, err)
 	}
 
 	return receipt, nil
