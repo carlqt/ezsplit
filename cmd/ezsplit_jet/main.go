335b8b0802deb161c999d676eb8d8e9d7c4add14
Carl Tablante <carlwilliam.tablante@gmail.com>
2024-07-11 17:19:19 +1000

    feat: Completed custom jet-go generate script

--- cmd/ezsplit_jet/main.go
+++ cmd/ezsplit_jet/main.go
@@ -3,13 +3,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"path"
 	"strconv"
 
 	"github.com/carlqt/ezsplit/internal"
 	"github.com/go-jet/jet/v2/generator/postgres"
-	"github.com/go-jet/jet/v2/generator/template"
-	postgres2 "github.com/go-jet/jet/v2/postgres"
 )
 
 func main() {
@@ -24,6 +25,8 @@ func main() {
 		panic(err)
 	}
 
+	dbSchema := "public"
+
 	dbConnection := postgres.DBConnection{
 		Host:     config.DBHost,
 		Port:     dbPort,
@@ -32,17 +35,44 @@ func main() {
 		SslMode:  "disable",
 
 		DBName:     config.DBName,
-		SchemaName: "dvds",
+		SchemaName: dbSchema,
 	}
 
 	err = postgres.Generate(
 		genDir,
 		dbConnection,
-		template.Default(postgres2.Dialect),
 	)
 
 	if err != nil {
 		log.Println("failed to generate models")
 		panic(err)
 	}
+
+	// After successful generation
+	// find the <schema> folder under .gen directory
+	srcDir := path.Join(genDir, config.DBName, dbSchema)
+	destDir := path.Join(genDir, dbSchema)
+
+	err = moveDir(srcDir, destDir)
+	if err != nil {
+		panic(err)
+	}
+
+	dbNameDir := path.Join(genDir, config.DBName)
+	os.RemoveAll(dbNameDir)
+	fmt.Printf("Generated models in %s\n", destDir)
+}
+
+func moveDir(src string, dest string) error {
+	err := os.RemoveAll(dest)
+	if err != nil {
+		return fmt.Errorf("failed to delete destination folder when moving: %w", err)
+	}
+
+	err = os.Rename(src, dest)
+	if err != nil {
+		return fmt.Errorf("failed to move %s to %s\n: %w", src, dest, err)
+	}
+
+	return nil
 }
